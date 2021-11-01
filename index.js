6e5c1d6802a8650fb9640f61051a20cd4f784ba0
Mohon45 <mdmohon6145@gmail.com>
2021-11-01 12:18:32 +0600

    update port added

--- index.js
+++ index.js
@@ -71,6 +71,10 @@ app.get('/', (req, res) => {
 
 });
 
+app.get('/hello', (req, res) =>{
+    res.send('Hello Update  server!');
+})
+
 app.listen(port, () => {
     console.log('Running Genius Server on port', port);
 })
